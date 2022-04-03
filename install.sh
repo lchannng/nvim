8a82ea22ce103029228fc2c1f254ceff4ce1ce3a
lchannng <l.channng@gmail.com>
2022-04-04 01:53:06 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -13,8 +13,16 @@ mkdir -p ~/.local/bin
 LS_ROOT=`pwd`/language-server
 
 cleanup ${LS_ROOT}/lua
-curl -L https://github.com/sumneko/lua-language-server/releases/download/2.6.7/lua-language-server-2.6.7-linux-x64.tar.gz | tar -zvx -C ${LS_ROOT}/lua
-ln -snf ${LS_ROOT}/lua/bin/lua-language-server ~/.local/bin/lua-language-server
+if [[ $OSTYPE == "linux-gnu"* ]]; then
+    curl -L https://github.com/sumneko/lua-language-server/releases/download/2.6.7/lua-language-server-2.6.7-linux-x64.tar.gz | tar -zvx -C ${LS_ROOT}/lua
+    ln -snf ${LS_ROOT}/lua/bin/lua-language-server ~/.local/bin/lua-language-server
+elif [[ $OSTYPE == "darwin"* ]]; then
+    brew update
+    brew install lua-language-server
+else
+    echo "unsupported os: $OSTYPE"
+    exit -1
+fi
 
 npm install --prefix ${LS_ROOT} pyright
 npm install --prefix ${LS_ROOT} bash-language-server
