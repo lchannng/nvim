394ec5df8e3cacc75f954d9878e0fa2947e4c8c5
lchannng <l.channng@gmail.com>
2023-03-01 10:37:26 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -178,7 +178,10 @@ require("lazy").setup({
       "hrsh7th/cmp-nvim-lua",
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
-      'L3MON4D3/LuaSnip',
+      {
+        'L3MON4D3/LuaSnip',
+        build = "make install_jsregexp",
+      },
       'saadparwaiz1/cmp_luasnip',
     }
   },
