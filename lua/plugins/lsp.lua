4b1db4ed098e114179411b65daa8a1fa7574a556
lchannng <l.channng@gmail.com>
2023-06-26 17:58:11 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -60,7 +60,7 @@ return {
                 globals = { "vim" },
               },
               format = {
-                enable = false,
+                enable = true,
               },
             },
           },
@@ -115,23 +115,22 @@ return {
       end
     end,
   },
-
   -- formatters
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.stylua,
-          -- nls.builtins.formatting.shfmt,
-        },
-      }
-    end,
-  },
+  -- {
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   event = { "BufReadPre", "BufNewFile" },
+  --   dependencies = { "mason.nvim" },
+  --   opts = function()
+  --     local nls = require("null-ls")
+  --     return {
+  --       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+  --       sources = {
+  --         nls.builtins.formatting.stylua,
+  --         -- nls.builtins.formatting.shfmt,
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- cmdline tools and lsp servers
   {
