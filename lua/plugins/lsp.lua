1aba3271194ef4a97077ced23b45ca7c3ead9b84
lchannng <l.channng@gmail.com>
2026-03-12 16:35:27 +0800

    add jsonls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,6 +39,21 @@ if vim.env.VIM_IDE then
     clangd = {},
     cmake = {},
     gopls = {},
+    jsonls = {
+      -- lazy-load schemastore when needed
+      before_init = function(_, new_config)
+        new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+        vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+      end,
+      settings = {
+        json = {
+          format = {
+            enable = true,
+          },
+          validate = { enable = true },
+        },
+      },
+    },
     lua_ls = {
       settings = {
         Lua = {
