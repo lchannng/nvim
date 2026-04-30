8aa8d338e2b68d03b253b15dcc8be08e9c1fa452
lchannng <l.channng@gmail.com>
2026-04-30 17:12:45 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,19 @@ local g = vim.g
 local o = vim.o
 
 g.do_filetype_lua = 1
+
+-- OSC 52 clipboard support (for SSH/tmux)
+g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 -- g.did_load_filetypes = 0
 
 -- don't load the plugins below
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = function()
       require("nvim-treesitter.install").update({ with_sync = true })
     end,
